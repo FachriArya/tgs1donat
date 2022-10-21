f9f3f972718389b325867b4a81176c01a6f04353
Muhamad Fachri arya Ramadhan <muhammadfachriaryaramadhan@gmail.com>
2022-10-21 22:13:55 +0700

    tgs1donat

---
+++ lib/constants.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const Color primaryColor = Color.fromARGB(255, 37, 105, 10);
+const Color bgColor = Color.fromARGB(255, 203, 255, 190);
+
+const double defaultPadding = 16.0;
+const double defaultBorderRadius = 12.0;
